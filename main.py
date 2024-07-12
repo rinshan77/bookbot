ff99bbb2341848293f19ce3f633ac4be452b1ce3
rinshan77 <simon.durakovic@proton.me>
2024-07-12 19:59:36 +0200

    7. This is a functioning program for now, ready to be submitted. I am aware it can be tweaked, but it's my first project, so...

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def count_words(book):
         count = 0
         for word in words:
             count += 1
-        print(f"The number of words in this book are {count}.")
+        print(f"The number of words in this document are {count}.")
+        return count
 
 def count_chars(book):
     lowercase_book = book.lower()
@@ -21,10 +22,28 @@ def count_chars(book):
 
 def print_char_count_table(char_counts, columns=10):
     items = list(char_counts.items())
-    print("The amount of each letter appearing in the book is:")
     for i in range(0, len(items), columns):
         row_items = items[i:i+columns]
         print(' '.join([f"{char}: {count:5}" for char, count in row_items]))
+
+def count_get(item):
+    return item[1]
+
+def sort_dictionary(char_counts):
+    char_tuple = char_counts.items()
+    char_list = list(char_tuple)
+    sorted_char_list = sorted(char_list, key=count_get, reverse=True)
+    return sorted_char_list
+
+def main(book):
+    print("--- Begin report of books/frankenstein.txt ---")  
+    word_count = count_words(book)
+    char_count = count_chars(book)
+    sorted_chars = sort_dictionary(char_count)
+    print(f"{word_count} words found in the document")
+    for char, count in sorted_chars:
+        print(f"The '{char}' character was found {count} times")
+    print("--- End report ---")
 
 if __name__ == "__main__":
     with open("books/frankenstein.txt") as f:
@@ -32,8 +51,9 @@ if __name__ == "__main__":
 
     #print(book)
     #count_words(book)
-    print(count_chars(book))
-    #print_char_counts_table(count_chars(book))
+    #print(count_chars(book))
+    #print_char_count_table(count_chars(book))
 
-#def main():
+main(book)
+
     
